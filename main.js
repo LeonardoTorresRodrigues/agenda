588cbd0a349c4128fe7c443af19ac07041222397
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2022-02-24 19:47:21 -0300

    Adicionando função para criar nota

---
+++ main.js
@@ -0,0 +1,27 @@
+import BotaoConclui from './components/taskDone.js'
+import BotaoDeleta from './components/taskDelete.js'
+
+const criarTarefa = (evento) => {
+
+    evento.preventDefault();
+    
+    const lista = document.querySelector('[data-list]');
+    const input = document.querySelector('[data-form-input]');
+    const valor = input.value;
+
+    const tarefa = document.createElement('li');
+    tarefa.classList.add('task');
+    const conteudo = `<p class="content">${valor}</p>`;
+
+    tarefa.innerHTML = conteudo;
+
+    tarefa.appendChild(BotaoConclui());
+    tarefa.appendChild(BotaoDeleta());
+    lista.appendChild(tarefa);
+    input.value = " ";
+    input.focus();
+} 
+
+const novaTarefa = document.querySelector('[data-form-button]');
+
+novaTarefa.addEventListener('click', criarTarefa);
