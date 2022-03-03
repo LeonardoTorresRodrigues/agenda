b589721eb82c203d4ef3a2e1872e5079742732bb
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2022-03-03 17:37:52 -0300

    Rafatorando

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 import BotaoConclui from './components/taskDone.js'
 import BotaoDeleta from './components/taskDelete.js';
-import BotaoCountdown from './components/addDate.js';
 
 const criarTarefa = (evento) => {
 
     evento.preventDefault();
+
+    let day = document.getElementById('day');
+    let dayValue = day.value;
+    let month = document.getElementById('month');
+    let monthValue = month.value;
+    let year = document.getElementById('year');
+    let yearValue = year.value;
+
+    let dateValue = document.createElement('p');
+    dateValue.classList.add('date');
     
     const lista = document.querySelector('[data-list]');
     const input = document.querySelector('[data-form-input]');
@@ -12,16 +21,20 @@ const criarTarefa = (evento) => {
 
     const tarefa = document.createElement('li');
     tarefa.classList.add('task');
-    const conteudo = `<p class="content">${valor}</p>`;
+    const conteudo = `<p class="content">${valor}</p>`
+    const dateContent = `<p class="date-content">Dia ${dayValue} do ${monthValue} de ${yearValue}</p>`
 
-    tarefa.innerHTML = conteudo;
+    tarefa.innerHTML = conteudo + dateContent;
 
     tarefa.appendChild(BotaoConclui());
     tarefa.appendChild(BotaoDeleta());
+    tarefa.appendChild(dateValue);
     lista.appendChild(tarefa);
     input.value = " ";
+    day.value = " ";
+    month.value = " ";
+    year.value = " ";
     input.focus();
-    BotaoCountdown();
 } 
 
 const novaTarefa = document.querySelector('[data-form-button]');
